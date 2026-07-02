259a7fa07f2f9b98b8c027b679792c6144c4f389
aymisbah <aymanmisbah78@gmail.com>
2026-07-02 14:19:37 -0400

    l3azwa

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
 cd /var/www/html
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
+
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
-php-fpm8.4 -F
+./wp-cli.phar config create \
+    --dbname="$DB_NAME" \
+    --dbuser="$DB_USER" \
+    --dbpass=$(cat /run/secrets/db_password) \
+    --dbhost=mariadb \
+    --allow-root
 
+./wp-cli.phar core install \
+    --url="https://$DOMAIN_NAME" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password=$(cat /run/secrets/wp_admin_password) \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --allow-root
+
+exec php-fpm8.4 -F
